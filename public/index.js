bbb5220117f05d244e11e4aa4e2816cd8012059c
Charley Lawton <user@CODECLAN055.local>
2016-09-30 21:33:14 +0100

    second commitr

--- public/index.js
+++ public/index.js
@@ -9,10 +9,25 @@ var requestComplete = function(){
   response = JSON.parse(jsonString);
   console.log(response);
 
+  var movie = function(movieName){
+    for(var movie in movies){
+      if (movie.name === movieName){
+        return movie;
+      }
+    }
+  }
+
+  var sumbit = document.querySelector('#inputText');
+  button.onclick = function(){
+
+  }
+
 }
-
+var printMovieName = function(movieName){
+  var movieNameText = document.getElementById('watching');
+  movieNameText.innerText = watching;
+}
 var makeRequest = function(url, callback){
-
   var request = new XMLHttpRequest();
   request.open("GET", url);
   request.onload = callback;
@@ -20,10 +35,15 @@ var makeRequest = function(url, callback){
 }
 
 var app = function(){
-  title = films[Math.floor(Math.random()*films.length)];
-      url = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&r=json";
-      makeRequest(url, requestComplete); 
+  var url = "http://localhost:3000";
+  var button = document.querySelector('button');
+  button.onclick = function(){
+    var input = document.getElementById('inputText'.value);
+    movieName = input;
+    makeRequest(url, requestComplete)
+  }
+  
 
 
 
-window.onload
+window.onload = app;
